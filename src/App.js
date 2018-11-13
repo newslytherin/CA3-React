6cdb17899436ae4e252d06bb480ac068a3db0e96
NikolajX4000 <supernikolaj@hotmail.com>
2018-11-13 14:12:31 +0100

    header

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import React, { Component } from "react"
+import { HashRouter as Router, Route, NavLink } from "react-router-dom"
 import Welcome from "./components/Welcome";
+import SWAPI from "./components/swapi";
 
-class App extends Component {
- constructor(props) {
-   super(props);
- }
- 
- render() {
-   return (
-     <div>
-       <Welcome />
-     </div>
-   )
- }
+export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {user: "Username"}
+  }
+  render() {
+    return (
+      <Router>
+        <div>
+          <ul className="header">
+            <li>{this.state.user}</li>
+            <li>
+              <NavLink exact to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/swapi">SWAPI</NavLink>
+            </li>
+            <li>
+              <NavLink to="/large-data-set">Large Data Set</NavLink>
+            </li>
+          </ul>
+          <hr />
+          <Route exact path="/" component={Welcome} />
+          <Route path="/swapi" component={SWAPI} />
+        </div>
+      </Router>
+    );
+  }
 }
-export default App;
