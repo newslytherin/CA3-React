7b3385479a8418383d874360c589622749b7123b
jack-borg <jacob.fam.borg@gmail.com>
2018-11-14 08:54:21 +0100

    swapi stuff

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react"
 import Welcome from "./components/Welcome";
+import Swapi from "./components/swapi";
 
 class App extends Component {
  constructor(props) {
@@ -10,6 +11,7 @@ class App extends Component {
    return (
      <div>
        <Welcome />
+       <Swapi />
      </div>
    )
  }
--- src/components/swapi.js
+++ src/components/swapi.js
@@ -4,34 +4,25 @@ import paginationFactory from 'react-bootstrap-table2-paginator';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import BootstrapTable from 'react-bootstrap-table-next';
 
+const URL = "https://swapi.co/api/people/";
+
 export default class swapi extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {swl:[props.swl]}
+        this.state = {swl:[]}
+    }
+
+    async componentDidMount(){
+        const swl = [] 
+        swl.push(await fetch(URL+"1/").then(res => res.json())) 
+        swl.push(await fetch(URL+"2/").then(res => res.json())) 
+        swl.push(await fetch(URL+"3/").then(res => res.json())) 
+        this.setState({swl})
     }
 
     render() {
-        const columns = [{
-            dataField: 'name',
-            text: 'Name',
-            sort: true
-        }, {
-            dataField: 'height',
-            text: 'Height',
-            sort: true
-        }, {
-            dataField: 'mass',
-            text: 'Weight',
-            sort: true
-        }, {
-            dataField: 'gender',
-            text: 'Gender',
-            sort: true
-        }, {
-            dataField: 'birth_year',
-            text: 'Birth Year',
-            sort: true
-        }];
+        const options = getOptions()
+        const columns = getColumns();
         return (
             <div>
                 <BootstrapTable
@@ -41,10 +32,53 @@ export default class swapi extends React.Component {
                     keyField='name'
                     data={this.state.swl}
                     columns={columns}
-                    pagination={paginationFactory()}
+                    pagination={paginationFactory(options)}
                     filter={filterFactory()}
                 />
             </div>
         )
     }
+}
+
+function getColumns() {
+    const columns = [{
+        dataField: 'name',
+        text: 'Name',
+        sort: true
+    }, {
+        dataField: 'height',
+        text: 'Height',
+        sort: true
+    }, {
+        dataField: 'mass',
+        text: 'Weight',
+        sort: true
+    }, {
+        dataField: 'gender',
+        text: 'Gender',
+        sort: true
+    }, {
+        dataField: 'birth_year',
+        text: 'Birth Year',
+        sort: true
+    }];
+    return columns;
+}
+
+function getOptions(total) {
+    return {
+        paginationSize: 5,
+        pageStartIndex: 1,
+        // hide: true, // Hide the  dropdown always
+        hidePageListOnlyOnePage: true, // Hide the pagination list when only one page
+        firstPageText: 'First',
+        prePageText: 'Back',
+        nextPageText: 'Next',
+        lastPageText: 'Last',
+        sizePerPageList: [{
+            text: '4', value: 4
+        }, {
+            text: 'All', value: (total !== 0) ? total : 10
+        }]
+    };
 }
