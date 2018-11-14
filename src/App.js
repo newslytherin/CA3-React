689920d6250a3d6b9f56455085aef0d8b7ca9315
NikolajX4000 <supernikolaj@hotmail.com>
2018-11-14 10:38:54 +0100

    added links to all pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react"
 import { HashRouter as Router, Route, NavLink } from "react-router-dom"
 import Welcome from "./components/Welcome";
+import User from "./components/User";
+import Admin from "./components/Admin";
 import SWAPI from "./components/swapi";
+import LargeDataSet from "./components/LargeDataSet";
 
 export default class App extends Component {
   constructor(props){
@@ -23,10 +26,19 @@ export default class App extends Component {
             <li>
               <NavLink to="/large-data-set">Large Data Set</NavLink>
             </li>
+            <li>
+              <NavLink to="/user">User</NavLink>
+            </li>
+            <li>
+              <NavLink to="/admin">Admin</NavLink>
+            </li>
           </ul>
           <hr />
           <Route exact path="/" component={Welcome} />
+          <Route path="/user" component={User} />
+          <Route path="/admin" component={Admin} />
           <Route path="/swapi" component={SWAPI} />
+          <Route path="/large-data-set" component={LargeDataSet} />
         </div>
       </Router>
     );
