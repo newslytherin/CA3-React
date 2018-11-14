6038c1c4732c4873f7620cb732c927e1544a6925
NikolajX4000 <supernikolaj@hotmail.com>
2018-11-14 21:26:46 +0100

    link to pages user and admin only shows if logged in with correct role

--- src/App.js
+++ src/App.js
@@ -29,8 +29,8 @@ export default class App extends Component {
             <li> <NavLink exact to="/">Home</NavLink> </li>
             <li> <NavLink to="/swapi">SWAPI</NavLink> </li>
             <li> <NavLink to="/large-data-set">Large Data Set</NavLink> </li>
-            {(this.state.roles.includes('user')) ? <li> <NavLink to="/user">User</NavLink> </li> : ''}
-            {(this.state.roles.includes('admin')) ? <li> <NavLink to="/admin">Admin</NavLink> </li> : ''}
+            {(this.state.loggedIn && this.state.roles.includes('user')) ? <li> <NavLink to="/user">User</NavLink> </li> : ''}
+            {(this.state.loggedIn && this.state.roles.includes('admin')) ? <li> <NavLink to="/admin">Admin</NavLink> </li> : ''}
           </ul>
           <hr />
           <Route exact path="/" render={() => <Welcome roles={this.state.roles} setroles={this.setroles} setname={this.setname} loggedIn={this.state.loggedIn} changeLoggedIn={this.changeLoggedIn} />} />
